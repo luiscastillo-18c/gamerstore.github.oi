f8ba7c5fedac2f84b4dac8adeee91de4701f3462
luiscastillo-18c <luis1284ufd4@gmail.com>
2026-05-17 16:01:39 -0600

    initial commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function cargar(seccion) {
+    const contenido = document.getElementById("contenido");
+
+    if (seccion === "home") {
+        contenido.innerHTML
+    }
+
+    if (seccion ==="historial"){
+        contenido.innerHTML = `
+        <section class="pagina">
+            <h1> historial vacio</h1>
+        </section>
+        
+        `;
+
+    }
+}
